b6a3d3258488e45748eb9a2496611844bb340753
Giorgos Mermigkis <giwrgosmerm@gmail.com>
2023-12-19 23:49:03 +0200

    loop unrolled using i,i+4,i+8,i+12

--- task1_simd/force1d_manual_sse.c
+++ task1_simd/force1d_manual_sse.c
@@ -24,34 +24,47 @@ float compute_force(float *positions, float x0)
 
     //we will be loading them 4 by 4 like SSE because 16 byte vectors
     //unroll the loop so the are being brought 8 by 8
-    for (size_t i = 0; i < N; i += 8) {
+    for (size_t i = 0; i < N; i += 16) {
         
         __m128 positions_vector0 = _mm_loadu_ps(&positions[i]);
         __m128 positions_vector1 = _mm_loadu_ps(&positions[i+4]);
+        __m128 positions_vector2 = _mm_loadu_ps(&positions[i+8]);
+        __m128 positions_vector3 = _mm_loadu_ps(&positions[i+12]);
 
         //float r = x0 - positions[i];
         __m128 r_vector0 = _mm_sub_ps(x0_vector, positions_vector0);
         __m128 r_vector1 = _mm_sub_ps(x0_vector, positions_vector1);
+        __m128 r_vector2 = _mm_sub_ps(x0_vector, positions_vector2);
+        __m128 r_vector3 = _mm_sub_ps(x0_vector, positions_vector3);
 
         //float r2 = r * r;
         __m128 r2_vector0 = _mm_mul_ps(r_vector0, r_vector0);
         __m128 r2_vector1 = _mm_mul_ps(r_vector1, r_vector1);
+        __m128 r2_vector2 = _mm_mul_ps(r_vector2, r_vector2);
+        __m128 r2_vector3 = _mm_mul_ps(r_vector3, r_vector3);
 
         //float s2 = rm2 / r2;
         __m128 s2_vector0 = _mm_div_ps(_mm_set1_ps(rm2), r2_vector0);
         __m128 s2_vector1 = _mm_div_ps(_mm_set1_ps(rm2), r2_vector1);
+        __m128 s2_vector2 = _mm_div_ps(_mm_set1_ps(rm2), r2_vector2);
+        __m128 s2_vector3 = _mm_div_ps(_mm_set1_ps(rm2), r2_vector3);
 
         //float s6 = s2*s2*s2;
         __m128 s6_vector0 = _mm_mul_ps(_mm_mul_ps(s2_vector0, s2_vector0), s2_vector0);
         __m128 s6_vector1 = _mm_mul_ps(_mm_mul_ps(s2_vector1, s2_vector1), s2_vector1);
-
+        __m128 s6_vector2 = _mm_mul_ps(_mm_mul_ps(s2_vector2, s2_vector2), s2_vector2);
+        __m128 s6_vector3 = _mm_mul_ps(_mm_mul_ps(s2_vector3, s2_vector3), s2_vector3);
 
         //force += 12 * eps * (s6*s6 - s6) / r;
         __m128 term_vector0 = _mm_div_ps(_mm_mul_ps(twelve_eps, _mm_sub_ps(_mm_mul_ps(s6_vector0, s6_vector0), s6_vector0)), r_vector0);
         __m128 term_vector1 = _mm_div_ps(_mm_mul_ps(twelve_eps, _mm_sub_ps(_mm_mul_ps(s6_vector1, s6_vector1), s6_vector1)), r_vector1);
+        __m128 term_vector2 = _mm_div_ps(_mm_mul_ps(twelve_eps, _mm_sub_ps(_mm_mul_ps(s6_vector2, s6_vector2), s6_vector2)), r_vector2);
+        __m128 term_vector3 = _mm_div_ps(_mm_mul_ps(twelve_eps, _mm_sub_ps(_mm_mul_ps(s6_vector3, s6_vector3), s6_vector3)), r_vector3);
 
         force_vec = _mm_add_ps(force_vec, term_vector0);
         force_vec = _mm_add_ps(force_vec, term_vector1);
+        force_vec = _mm_add_ps(force_vec, term_vector2);
+        force_vec = _mm_add_ps(force_vec, term_vector3);
        //force += term_vector[0] + term_vector[1] + term_vector[2] + term_vector[3];
     }
 
@@ -70,7 +83,7 @@ int main(int argc, const char** argv)
 
 	//declare,malloc && init positions
     //allign memory
-	float *positions = (float*)_mm_malloc(N * sizeof(float), 32);
+	float *positions = (float*)_mm_malloc(N * sizeof(float), 64);
 
 	for (size_t i=0; i<N; i++)
 		positions[i] = drand48()+0.1;
@@ -98,7 +111,8 @@ int main(int argc, const char** argv)
         printf("Force acting at x_0=%lf : %lf\n", x0[j], f0[j]/repetitions);
 
     printf("elapsed time: %lf mus\n", 1e6*(end-start));
-	return 0;
+	
     _mm_free(positions);
+    return 0;
 }
 
